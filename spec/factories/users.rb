e5c42e38fe5f30a7e4aa6f81070743eaf8a2a487
vanja-videnov <vanja.videnov@rbt.rs>
2016-03-14 16:18:57 +0100

    Add validations for user model fields with spec for it Add factories for waiter_user Add coverage files for testing with it

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :user_waiter, class: User do
+    name 'Sanja Sanja'
+    email 'sanja@rbt.com'
+    telephone '1234567890'
+    password '123456vv'
+    manager false
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,147 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.new(email: email, password: password, telephone: telephone, name: name, manager: manager)}
+  let(:email) { 'vanja@rbt.com' }
+  let(:password) { '123456vv' }
+  let(:telephone) { '0643335504' }
+  let(:name) { 'Vanja' }
+  let(:manager) { true}
+  it { expect(user).to be_valid }
+
+  shared_examples_for 'an invalid user' do
+    before do
+
+      subject.validate  #for creating errors
+    end
+    it { expect(subject).to be_invalid }
+    it 'should show errors' do
+      expect(subject.errors).to be_present
+    end
+  end
+  shared_examples_for 'a valid user' do
+    it { should be_valid }
+  end
+
+  describe '#email' do
+
+    context 'when too long' do
+      let(:email) { 'reallyreallyreallyreallyreallyreallyreallyreallylongemailthatiscrazyemailbutisisjustfortest@rbt.com' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when too short' do
+      let(:email) {  's@s.s' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is not present' do
+      let(:email) { '' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is not ok format' do
+      let(:email) { 'vanjaaaaa' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is not unique' do
+
+      before do
+        create(:user_waiter)
+      end
+      let(:email) { 'sanja@rbt.com' }
+
+      it_behaves_like 'an invalid user'
+    end
+  end
+
+  describe '#password' do
+
+    context 'when too short' do
+      let(:password) { 'bla' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is not present' do
+      let(:password) { '' }
+
+      it_behaves_like 'an invalid user'
+    end
+  end
+
+  describe '#telephone' do
+
+    context 'when is invalid format' do
+      let(:telephone) { '44bhbhhb' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is nil' do
+      let(:telephone) {nil}
+
+      it_behaves_like 'a valid user'
+    end
+
+    context 'when is empty string' do
+      let(:telephone) {''}
+
+      it_behaves_like 'a valid user'
+    end
+  end
+
+  describe '#name' do
+
+    context 'when is nil' do
+      let(:name) { nil }
+
+      it_behaves_like 'a valid user'
+    end
+
+    context 'when is empty string' do
+      let(:name) { '' }
+
+      it_behaves_like 'a valid user'
+    end
+  end
+
+  describe '#manager' do
+
+    context 'when is boolean value' do
+      let(:manager) { true }
+
+      it_behaves_like 'a valid user'
+    end
+
+    context 'when is 1 or 0 value' do
+      let(:manager) { 1 }
+
+      it_behaves_like 'a valid user'
+    end
+
+    context 'when is empty' do
+      let(:manager) { '' }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is nil' do
+      let(:manager) { nil }
+
+      it_behaves_like 'an invalid user'
+    end
+
+    context 'when is not boolean value' do
+      let(:manager) { 'bdskjbdjksbds' }
+
+      it_behaves_like 'a valid user'
+    end
+  end
+
 end
